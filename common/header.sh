5f3994e304f3efb190ad11b51f37f6b502e8356c
PS2ClassicsVault <118215758+PS2ClassicsVault@users.noreply.github.com>
2025-08-27 16:02:31 -0500

    Add files via upload

--- common/header.sh
+++ common/header.sh
@@ -1,1 +1,14 @@
+#
+# Addon.d script created from AFZC tool by Nikhil Menghani
+#
 
+ps | grep zygote | grep -v grep >/dev/null && BOOTMODE=true || BOOTMODE=false
+$BOOTMODE || ps -A 2>/dev/null | grep zygote | grep -v grep >/dev/null && BOOTMODE=true
+
+# [ ! $BOOTMODE ] && [ -z "$2" ] && exit
+. /tmp/backuptool.functions
+
+crDroidappsDir="/sdcard/crDroidapps"
+addonDir="$crDroidappsDir/addonLogs"
+crDroidappsAddonLogFile="$addonDir/crDroidapps_addon_$(date +%Y_%m_%d).log"
+mkdir -p "$(dirname $crDroidappsAddonLogFile)";
